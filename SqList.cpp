8b9225e4cf4e4a5b6866bb1ef9cf14fd01d37679
lovingd-85 <481889613@qq.com>
2023-09-27 19:04:39 +0800

    创建顺序列表

---
+++ SqList.cpp
@@ -0,0 +1,15 @@
+// Created on iPad.
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MaxSize 15
+
+typedef struct{
+    int *data;
+    int length;
+}
+
+int main() {
+    printf("Hello World!");
+    return 0;
+}
